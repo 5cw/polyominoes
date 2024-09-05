cacc11c884dc63dcce5d2c6ee647cf824212f72b
Lexi <kreinsbo@uvm.edu>
2024-09-05 16:20:34 -0400

    init

---
+++ polyominoes.md
@@ -0,0 +1,22 @@
+# Hexomino naming
+
+I was watching [this video](https://youtu.be/39YYZcwCuv0?si=u7-G3ntPZl3g7kQj&t=658) when the creator mentioned that the hexominoes didn't have official names. I looked around and I saw a [couple of](https://gamepuzzles.com/sxnames.htm) [attempts](https://conwaylife.com/wiki/Polyomino) to assign letters to the free
+hexominoes, but I felt unsatisfied.
+
+I always liked the tetris pieces and their lettering.
+
+![tetrominoes](images/tetrominoes.png)
+
+And the tetris pieces are one-sided, i.e. they distinguish between reflections, but not rotations. 
+
+The pentominoes are also named with letters, and all of them pretty much look like their letters too. A couple of the chiral pentominoes have reflections with obvious letters, a couple are listed on wikipedia as `A'` where `A` is the reflection. I came up with some reasonable substitutes which are a bit of a stretch, but I like that they all have names.
+
+![pentominoes](images/pentominoes.png)
+
+But there are 35 free hexominoes, which can be given letters if you add some complexity, but I wanted to name all 60 one-sided hexominoes, which would take more than all the lower and uppercase letters. So I decided to use one-syllable words to describe their shapes. I like this as a solution because it makes them visual and memorable, and the reflections can have related names, and also they can coexist with the pentominoes or tetrominoes without overlap.
+
+![hexominoes](images/hexominoes.png)
+
+The first 50 are all pairs, and I tried to name them such that the one 'facing' to the right (no hard and fast rule, just off vibes and what I'm picturing them as) has a name which is in some way before, more common, larger, or senior to the one facing to the left. For instance, the animals I tried to name with adult and child terms ('peep' for bird child isn't universal but is cute, and 'asp' is just another more obscure term for some kinds of snake) The naming process involved a lot of googling '______ synonyms' but I think it turned out ok!
+
+Open to input I guess. Just had the urge.
---
+++ polyominoes.py
@@ -0,0 +1,147 @@
+from matplotlib import pyplot as plt, patches
+import pandas as pd
+
+
+cmap = plt.get_cmap('hsv', 100)
+
+def norm(squares):
+    xs, ys = zip(*squares)
+    return {(x - min(xs), y - min(ys)) for x, y in squares}
+def rot(squares):
+    return norm([(-y, x) for x, y in squares])
+
+def flip(squares):
+    return norm([(-x, y) for x, y in squares])
+
+def bitrep(squares):
+    return sum(1 << x + y * 8 for x, y in squares)
+
+def decode_bitrep(b):
+    sq = set()
+    for i in range(64):
+        y, x = divmod(i, 8)
+        if b >> i & 1:
+            sq.add((x, y))
+    return sq
+class Polyomino:
+    squares: set[(int, int)]
+    rots: list[int]
+    draw_rot: int
+    name: str
+
+    def __init__(self, squares, name=''):
+        if not isinstance(squares, int):
+            self.squares = norm(squares)
+        else:
+            self.squares = decode_bitrep(squares)
+        r1 = rot(self.squares)
+        r2 = rot(r1)
+        r3 = rot(r2)
+        self.rots = [*map(bitrep, [self.squares, r1, r2, r3])]
+        self.draw_rot = 0
+        self.name = name
+
+    def extensions(self):
+        checked = set(self.squares)
+        out = []
+        for x, y in self.squares:
+            for dx, dy in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+                nx, ny = x + dx, y + dy
+                if (nx, ny) not in checked:
+                    out.append(Polyomino(self.squares | {(nx, ny)}))
+                    checked.add((nx, ny))
+        return out
+
+
+    def __eq__(self, other):
+        return other.rots[0] in self.rots
+
+    def __repr__(self):
+        return str(self.squares)
+
+    def draw(self, ax):
+        sq = self.squares
+        for i in range(self.draw_rot):
+            sq = rot(sq)
+        xs, ys = zip(*sq)
+        mx, my = max(xs) + 1, max(ys) + 1
+        cx, cy = mx / 2, my / 2
+        color = cmap(self.rots[self.draw_rot]% 97)
+        for x, y in sq:
+            ax.add_patch(patches.Rectangle((x - cx, y - cy), 1, 1, facecolor=color, edgecolor='black'))
+        ax.set_axis_off()
+        ax.set_xlim(-3.5, 3.5)
+        ax.set_ylim(-3.5, 3.5)
+        ax.set_title(f'{self.name}', y=-.2)
+
+PREFIXES = ['null', 'mon', 'd', 'tri', 'tetr', 'pent', 'hex']
+
+
+def get_name(n):
+    return PREFIXES[n] + 'omino'
+
+
+def generate(n: int):
+    nominoes = [Polyomino([(0, 0)])]
+    for _ in range(n - 1):
+        #print(_)
+        newnominoes = []
+        for p in nominoes:
+            for x in p.extensions():
+                if x not in newnominoes:
+                    newnominoes.append(x)
+        nominoes = newnominoes
+    #print(nominoes, len(nominoes))
+
+
+
+    df = pd.read_csv(f'data/{get_name(n)}es.csv')
+    df.fillna(0, inplace=True)
+
+    sv = pd.DataFrame(columns=['name', 'bitrep'])
+
+    used = set()
+    for i, nomino in enumerate(nominoes):
+        nomino.draw_rot = int(df['rot'].get(i, 0))
+        nomino.name = df['name'][i]
+    idx = 0
+    for i, nomino in enumerate(nominoes):
+        if not df['main'][i]:
+            continue
+
+        f = bitrep(flip(nomino.squares))
+        for j, o in enumerate(nominoes):
+            if f in o.rots:
+                sv.loc[idx] = [nomino.name, nomino.rots[nomino.draw_rot]]
+                sv.loc[idx + 1] = [o.name, o.rots[o.draw_rot]]
+                used |= {i, j}
+                idx += 2
+
+    for i, nomino in enumerate(nominoes):
+        if i in used:
+            continue
+        sv.loc[idx] = [nomino.name, nomino.rots[nomino.draw_rot]]
+        idx += 1
+    sv['bitrep'] = sv['bitrep'].astype(int)
+    sv.to_csv(f'data/{get_name(n)}_data.csv', index=False)
+
+def make_image(n):
+    rows, cols = [(0, 0), (1, 1), (1, 1), (1, 2), (1, 7), (3, 6),(6, 10)][n]
+
+    bfig, axs = plt.subplots(rows, cols, figsize=(cols, rows), dpi=300)
+    sv = pd.read_csv(f'data/{get_name(n)}_data.csv')
+
+    for i, [name, b] in sv.iterrows():
+        i = int(i)
+        p = Polyomino(int(b), name)
+        if rows == 1:
+            ax = axs[i]
+        else:
+            ax = axs[i // cols, i % cols]
+        ax.set_aspect('equal')
+        p.draw(ax)
+    bfig.savefig(f'images/{get_name(n)}es.png',bbox_inches='tight')
+
+N = 6
+generate(N)
+make_image(N)
